f2d653e2441b4cd5413ba0b14e35536a70dd2029
clayton j mosher <mosherclayton@gmail.com>
2019-02-08 04:00:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,26 +1,33 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
 function addElementToBeginningOfArray(array, element){
  return [element, ...array];
 }
+
 function destructivelyAddElementToBegginningOfArray(array, element){
   array.unshift(element);
   return array
 }
+
 function accessElementInArray(array, index){
   return array[index]
 }
+
 function destructivelyRemoveElementFromBegginingOfArray(array){
   array.shift()
   return array
 }
+
 function removeElementFromBeginningOfArray(array){
   array.slice(1)
   return array
 }
+
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
   return array
 }
+
 function removeElementFromEndOfArray(array){
   array.slice(0,array.length - 1)
   return array
