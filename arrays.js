1371ce9f368c49c8259cab1903ccf63105a34773
clayton j mosher <mosherclayton@gmail.com>
2019-02-08 04:08:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBegginningOfArray(array, element){
-  array.unshift(element);
+  array.unshift(element)
   return array
 }
 
